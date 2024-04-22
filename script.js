658d719256f7ed5943f1070a18d8edc32a200cde
bima-taruna <bimataruna21@gmail.com>
2024-04-22 16:49:05 +0700

    give cell on gameboard functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function Player(params) {
+function Player(params, symbol) {
   let playerName = params;
   let score = 0;
+  let playerSymbol = symbol;
 
   const setPlayer = (params) => {
     playerName = params;
@@ -12,21 +13,22 @@ function Player(params) {
 
   const getScore = () => score;
 
+  const getSymbol = () => playerSymbol;
+
   return {
     getName,
     setPlayer,
     increaseScore,
     getScore,
+    getSymbol,
   };
 }
 
 function Gameboard() {
-  const board = [];
+  let board = new Array(9).fill("");
 
   const resetBoard = () => {
-    for (let i = 1; i <= 9; i++) {
-      board.push("");
-    }
+    board = new Array(9).fill("");
   };
 
   const getBoard = () => board;
@@ -35,9 +37,75 @@ function Gameboard() {
     board[index] = params;
   };
 
+  const getField = (num) => board[num];
+
   return {
     resetBoard,
     getBoard,
     insert,
+    getField,
   };
 }
+
+let game = (function GameController() {
+  const cells = document.querySelectorAll(".cell");
+  const winningCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const players = [];
+  const board = Gameboard();
+  let activePlayer;
+
+  cells.forEach((item) => {
+    item.addEventListener("click", cellClicked);
+  });
+
+  initializePlayer();
+
+  function initializePlayer() {
+    let playerOneName = prompt("insert player one name : ", "PlayerOne");
+    let playerOne = Player(playerOneName, "X");
+    players[0] = playerOne;
+    let playerTwoName = prompt("insert player two name : ", "PlayerTwo");
+    let playerTwo = Player(playerTwoName, "O");
+    players[1] = playerTwo;
+    activePlayer = players[0];
+  }
+
+  function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+    if (board.getField(cellIndex) != "") {
+      return;
+    }
+    updateCell(this, cellIndex);
+    switchPlayerTurn();
+    checkWin();
+  }
+
+  function updateCell(cell, index) {
+    board.insert(index, activePlayer.getSymbol());
+    cell.textContent = activePlayer.getSymbol();
+  }
+
+  function switchPlayerTurn() {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    printNewRound();
+  }
+
+  function getActivePlayer() {
+    return activePlayer;
+  }
+
+  function printNewRound() {
+    console.log(`${getActivePlayer().getName()}'s turn .`);
+  }
+
+  function checkWin() {}
+})();
